d7be421767387e8863ea89b16eb74ef002bb74a3
ArranCheung <24ac0736@barton.ac.uk>
2024-12-15 19:11:53 +0000

    Add files via upload

---
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WeatherApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.search = new System.Windows.Forms.TextBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+			this.flowLayoutPanel1.Controls.Add(this.panel1);
+			this.flowLayoutPanel1.Controls.Add(this.panel2);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(942, 636);
+			this.flowLayoutPanel1.TabIndex = 3;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Transparent;
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.search);
+			this.panel1.Location = new System.Drawing.Point(3, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(936, 124);
+			this.panel1.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.textBox1.Font = new System.Drawing.Font("Niagara Engraved", 47F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(177, 9);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(565, 65);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.Text = "Weather";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button1.Location = new System.Drawing.Point(632, 89);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Search";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// search
+			// 
+			this.search.Location = new System.Drawing.Point(214, 91);
+			this.search.Name = "search";
+			this.search.Size = new System.Drawing.Size(362, 20);
+			this.search.TabIndex = 0;
+			this.search.TextChanged += new System.EventHandler(this.search_TextChanged);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Transparent;
+			this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.panel2.Controls.Add(this.tableLayoutPanel1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel2.Location = new System.Drawing.Point(3, 133);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(936, 460);
+			this.panel2.TabIndex = 2;
+			this.panel2.Visible = false;
+			this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.96651F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.03349F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 178F));
+			this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.chart1, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
+			this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(21, 19);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.142857F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.912442F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.66821F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(894, 434);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
+			this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+			this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.tableLayoutPanel1.SetRowSpan(this.richTextBox1, 4);
+			this.richTextBox1.Size = new System.Drawing.Size(323, 428);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.tableLayoutPanel1.SetColumnSpan(this.comboBox1, 2);
+			this.comboBox1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold);
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Temperature",
+            "Wind speed",
+            "Pollutent levels"});
+			this.comboBox1.Location = new System.Drawing.Point(332, 3);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(559, 23);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.Text = "View other data";
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// chart1
+			// 
+			this.chart1.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.chart1.BackSecondaryColor = System.Drawing.Color.AntiqueWhite;
+			this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+			this.chart1.BorderlineWidth = 0;
+			this.chart1.BorderSkin.BorderWidth = 0;
+			this.tableLayoutPanel1.SetColumnSpan(this.chart1, 2);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(332, 64);
+			this.chart1.Name = "chart1";
+			this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+			this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.tableLayoutPanel1.SetRowSpan(this.chart1, 2);
+			this.chart1.Size = new System.Drawing.Size(559, 367);
+			this.chart1.TabIndex = 2;
+			this.chart1.Click += new System.EventHandler(this.chart1_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.tableLayoutPanel1.SetColumnSpan(this.comboBox2, 2);
+			this.comboBox2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold);
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "Particulate matter PM10",
+            "Particulate matter PM2.5",
+            "Carbon monoxide",
+            "Carbon dioxide",
+            "Nitrogen dioxide",
+            "Sulpher dioxide"});
+			this.comboBox2.Location = new System.Drawing.Point(332, 34);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(559, 23);
+			this.comboBox2.TabIndex = 3;
+			this.comboBox2.Text = "Select view";
+			this.comboBox2.Visible = false;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Moccasin;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(942, 636);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox search;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ WeatherApp/WeatherApp/Form1.cs
@@ -0,0 +1,603 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WeatherApp
+{
+	public partial class Form1 : Form
+	{
+		weather Rq1 = new weather();
+		public string weatherPropertiesSelection = "";
+		public string weatherPollutentSelection = "";
+		public string weatherWindSelection = "";
+
+		public List<Series> PollutentGraphInfo = new List<Series>();
+		public List<Series> WindGraphInfo = new List<Series>();
+		public List<string> pollutentNames = new List<string> { "Particulate matter PM10", "Particulate matter PM2.5", "Carbon monoxide", "Carbon dioxide", "Nitrogen dioxide", "Sulpher dioxide" };
+		List<string> windNames = new List<string> { "Wind (10m)", "Wind (80m)", "Wind (120m)", "Wind (180m)", "Wind gusts (10m)" };
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public void changeDataDisplayed(string choice) // Updating the textboxes when the menu selection has been changed
+		{
+			string currentTime = DateTime.Now.ToString();
+			currentTime = currentTime.Substring(currentTime.IndexOf(' ') + 1, 2);
+
+			switch (choice)
+			{
+				case "Pollutent levels":
+					// change the items in combo menu
+					comboBox2.Visible = true;
+					comboBox2.Items.Clear();
+					comboBox2.Items.AddRange(pollutentNames.ToArray());
+
+					string textBox = "";
+
+					foreach (string item in Rq1.PollutentData.Keys)
+					{
+						textBox += (item + "\n");
+						List<string> data = Rq1.PollutentData[item].Split(',').ToList();
+
+						textBox += data[Convert.ToInt32(currentTime) - 1] + "\n\n";
+					}
+
+					richTextBox1.Text = textBox;
+
+
+					// making the graph
+
+					PollutentGraphInfo.Clear();
+					foreach (string item in Rq1.PollutentData.Keys)
+					{
+						int counter = 0;
+						Series DataSeries = new Series();
+
+						for (int i = 0; i < 24; i++)
+						{
+							string data = Rq1.PollutentData[item].Split(',')[i];
+							DataSeries.Points.AddXY(Rq1.Times[counter].Substring(Rq1.Times[counter].IndexOf('T') + 1, 5), Rq1.PollutentData[item].Split(',')[counter]);
+							counter++;
+						}
+						PollutentGraphInfo.Add(DataSeries);
+					}
+
+					try
+					{
+						while (true)
+						{
+							chart1.Series.RemoveAt(0);
+							chart1.ChartAreas.RemoveAt(0);
+						}
+					}
+					catch { }
+
+					if (weatherPollutentSelection != "")
+					{
+						int index = pollutentNames.IndexOf(weatherPollutentSelection);
+						chart1.Series.Add(PollutentGraphInfo[index]);
+						chart1.Series[0].Name = Rq1.PollutentData.Keys.ElementAt(index);
+						chart1.ChartAreas.Add(new ChartArea());
+					}
+					else
+					{
+						chart1.Series.Add(PollutentGraphInfo[0]);
+						chart1.Series[0].Name = Rq1.PollutentData.Keys.ElementAt(0);
+						chart1.ChartAreas.Add(new ChartArea());
+					}
+
+					break;
+
+
+				case "Temperature":
+					comboBox1.SelectedItem = "Temperature";
+
+					Rq1.temperature();
+
+					string weatherText = $"Time\t\t\tTemperature\n";
+					Series series = new Series();
+					series.ChartType = SeriesChartType.Line;
+					series.Name = Rq1.Place;
+
+					for (int i = 0; i < 24; i++)
+					{
+						string time = Rq1.timeStamps.ElementAt(i).Key.Substring(Rq1.timeStamps.ElementAt(i).Key.IndexOf('T') + 1, 5);
+						float temp = Rq1.timeStamps.ElementAt(i).Value;
+
+						weatherText += $"{time}\t\t\t{temp}\n";
+
+						// creating chart
+						series.Points.AddXY(time, temp);
+					}
+					try
+					{
+						chart1.Series.RemoveAt(0);
+						chart1.ChartAreas.RemoveAt(0);
+					}
+					catch { }
+					chart1.Series.Add(series);
+					chart1.ChartAreas.Add(new ChartArea());
+
+					richTextBox1.Text = weatherText;
+
+					break;
+
+
+				case "Wind speed":
+					// chaning items in combo menu
+					comboBox2.Visible = true;
+					comboBox2.Items.Clear();
+					comboBox2.Items.AddRange(windNames.ToArray());
+
+					// updating the textbox
+					textBox = "";
+
+					for (int i = 0; i < Rq1.WindData.Keys.Count; i++)
+					{
+						List<string> list = new List<string> { "Wind speed:", "Wind direction:" };
+
+						string type = Rq1.WindData.Keys.ElementAt(i);
+						string[] data = Rq1.WindData[type];
+
+						textBox += type + "\n";
+
+						foreach (string item in data)
+						{
+							textBox += ($"{list[0]} {item.Split(',')[Convert.ToInt32(currentTime)]}\n");
+							list.RemoveAt(0);
+						}
+
+						textBox += "\n";
+					}
+					richTextBox1.Text = textBox;
+
+
+					// updating the graph
+
+					foreach (string[] Wind in Rq1.WindData.Values)
+					{
+						foreach (string WindProperty in Wind)
+						{
+							string[] sp = WindProperty.Split(',');
+							Series WindSeries = new Series();
+
+							for (int i = 0; i < 24; i++)
+							{
+								string time = Rq1.timeStamps.ElementAt(i).Key.Substring(Rq1.timeStamps.ElementAt(i).Key.IndexOf('T') + 1, 5);
+								string data = sp[i];
+
+								WindSeries.Points.AddXY(time, data);
+							}
+							WindGraphInfo.Add(WindSeries);
+						}
+					}
+
+					try
+					{
+						while (true)
+						{
+							chart1.Series.RemoveAt(0);
+							chart1.ChartAreas.RemoveAt(0);
+						}
+					}
+					catch { }
+
+					if (weatherWindSelection != "")
+					{
+						int index = windNames.IndexOf(weatherWindSelection);
+						chart1.Series.Add(WindGraphInfo[index * 2]);
+						chart1.Series[0].Name = windNames[index];
+						chart1.ChartAreas.Add(new ChartArea());
+						comboBox2.SelectedItem = windNames[index];
+					}
+					else
+					{
+						chart1.Series.Add(WindGraphInfo[0]);
+						chart1.Series[0].Name = windNames[0];
+						chart1.ChartAreas.Add(new ChartArea());
+						comboBox2.SelectedIndex = 0;
+					}
+
+					break;
+
+
+				case "Rain probability":
+					break;
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e) // when the search button is clicked
+		{
+			string text = search.Text;
+			panel2.Visible = true;
+
+			// call api and get info
+			richTextBox1.Text = text;
+			Rq1.Place = text;
+			bool valid = Rq1.start();
+
+			if (valid)
+			{
+				if (weatherPropertiesSelection != "")
+				{
+					Rq1.changeType(weatherPropertiesSelection);
+					changeDataDisplayed(weatherPropertiesSelection);
+				}
+				else
+				{
+					Rq1.changeType("Temperature");
+					changeDataDisplayed("Temperature");
+				}
+			}
+			else
+			{
+				richTextBox1.Text = "Location too specific";
+			}
+
+		}
+
+		private void search_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void panel2_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // when the selection box is changed
+		{
+			comboBox2.Visible = false;
+			comboBox2.SelectedItem = null;
+
+			string choice = comboBox1.SelectedItem.ToString();
+
+			Rq1.changeType(choice);
+			changeDataDisplayed(choice);
+		}
+
+		private void chart1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (comboBox2.SelectedItem == null) return;  
+			string choice = comboBox2.SelectedItem.ToString();
+
+			// clearing the chart out
+			try
+			{
+				while (true)
+				{
+					chart1.Series.RemoveAt(0);
+					chart1.ChartAreas.RemoveAt(0);
+				}
+			}
+			catch { }
+
+			try
+			{
+				// adding sources to the chart
+				if (comboBox1.SelectedItem == "Pollutent levels")
+				{
+					int index = pollutentNames.IndexOf(choice);
+					weatherPollutentSelection = choice;
+					chart1.Series.Add(PollutentGraphInfo[index]);
+				}
+				else if (comboBox1.SelectedItem == "Wind speed")
+				{
+					int index = windNames.IndexOf(choice);
+					weatherWindSelection = choice;
+					chart1.Series.Add(WindGraphInfo[index]);
+				}
+				
+				chart1.ChartAreas.Add(new ChartArea());
+				chart1.Series[0].Name = choice;
+			}
+			catch { }
+		}
+	}
+
+	public class weather
+	{
+		public string Place { get; set; }
+		public weather(string city)
+		{
+			Place = city;
+		}
+		public weather()
+		{
+			Place = "";
+		}
+
+		public string longitude { get; set; }
+		public string latitude { get; set; }
+		public string elevation { get; set; }
+		public string population { get; set; }
+		public string timezone { get; set; }
+		public string country { get; set; }
+		public Dictionary<string, float> timeStamps { get; set; }
+		public Dictionary<string, string> PollutentData { get; set; }
+		public Dictionary<string, string[]> WindData { get; set; }
+		public Dictionary<string,string> RaindData { get; set; }
+		public List<string> Times { get; set; }
+
+		public void changeType(string type) // gets the relavent information and subroutines required for that information to be fetched
+		{
+			switch (type)
+			{
+				case "Temperature":
+					temperature();
+					break;
+				// make more types for pollen and wind speed etc
+				case "Pollutent levels":
+					pollutents();
+					break;
+				case "Rain probability":
+					rain();
+					break;
+				case "Wind speed":
+					wind();
+					break;
+			}
+			Debug.WriteLine("type change");
+		}
+		public string makeRq(string path) // formulates a request to the api
+		{
+			//formatting and making request
+			WebRequest request = WebRequest.Create(path);
+			request.Method = WebRequestMethods.Http.Get;
+			request.ContentType = "application/json; charset=utf-8";
+			string text;
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+			// reading info from file
+			using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+			{
+				text = reader.ReadToEnd();
+			}
+
+			return text;
+		}
+
+		public bool start() // gets the relevant information about the searched place
+		{
+
+			string text = makeRq($"https://geocoding-api.open-meteo.com/v1/search?name={Place}&count=1&language=en&format=json");
+
+			//formatting text
+			try
+			{
+				text = text.Substring(text.IndexOf('['), text.Length - text.IndexOf('['));
+				text = text.Substring(2, text.Length - (text.Length - text.IndexOf(']')) - 3);
+
+				string[] info = text.Split(',');
+				Dictionary<string, string> KeyInfo = new Dictionary<string, string>();
+
+				foreach (string s in info)
+				{
+					string final = "";
+					foreach (char c in s)
+					{
+						if (c != '"') final += c;
+					}
+					if (final.StartsWith("postcodes") || !final.Contains(":")) continue;
+
+					string key = final.Split(':')[0];
+					string value = final.Split(':')[1];
+
+					KeyInfo.Add(key, value);
+
+				}
+
+				// storing info in class
+				longitude = KeyInfo["longitude"];
+				latitude = KeyInfo["latitude"];
+				elevation = KeyInfo["elevation"];
+				timezone = KeyInfo["timezone"];
+				try
+				{
+					population = KeyInfo["population"];
+				}
+				catch { }
+
+				return true;
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				return false;
+			}
+
+		}
+
+		public bool temperature() // gets the temperature
+		{
+			try
+			{
+				// creating weather request
+				string weather = makeRq($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m");
+
+				// formatting text
+				string originaltime = weather.Substring(weather.IndexOf('['), weather.Length - weather.IndexOf('['));
+				string times = originaltime.Substring(2, originaltime.Length - (originaltime.Length - originaltime.IndexOf(']')) - 3);
+
+				string temps = weather.Replace(times, string.Empty);
+				string[] sp = temps.Split(':');
+				temps = sp.Last();
+				temps = temps.Substring(1, temps.Length - 4);
+
+				// creating lists
+				string[] Times = times.Split(',');
+				string[] Temperature = temps.Split(',');
+
+
+				// adding the times to a dictionary with their temperatures
+				Dictionary<string, float> TimeTemps = new Dictionary<string, float>();
+
+				for (int i = 0; i < Temperature.Length; i++)
+				{
+					TimeTemps.Add(Times[i], float.Parse(Temperature[i]));
+				}
+				timeStamps = TimeTemps;
+				return true;
+			}
+			catch (ArgumentOutOfRangeException) { return false; }
+		}
+		public void wind()
+		{
+			try
+			{
+				WindData = new Dictionary<string, string[]>();
+
+				string weather = makeRq($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m");
+
+				int start = weather.LastIndexOf("wind_speed_10m");
+				weather = weather.Substring(start);
+
+				List<string> windData = new List<string>();
+				List<string> windNames = new List<string> { "Wind (10m)", "Wind (80m)", "Wind (120m)", "Wind (180m)", "Wind gusts (10m)" };
+
+				while (true)
+				{
+					try
+					{
+						bool inside = false; string dataToAdd = "";
+
+						foreach (char item in weather)
+						{
+							if (item == '[' || item == ']')
+							{
+								if (inside)
+								{
+									inside = false;
+									windData.Add(dataToAdd);
+									dataToAdd = "";
+								}
+								else { inside = true; }
+								continue;
+							}
+							if (inside)
+							{
+								dataToAdd += item;
+							}
+						}
+						break;
+					}
+					catch
+					{
+						break;
+					}
+				}
+
+				for (int i = 0; i < 4; i++)
+				{
+					WindData.Add(windNames[i], new string[] { windData[i], windData[i + 4] });
+				}
+				WindData.Add(windNames[4], new string[] { windData[4] });
+			}
+			catch { }
+		}
+		public void rain()
+		{
+			try
+			{
+				RaindData = new Dictionary<string, string>();
+
+				string weather = makeRq($"");
+			}
+			catch { }
+		}
+		public void pollutents() // stores the pollutents
+		{
+			try
+			{
+				PollutentData = new Dictionary<string, string>();
+
+				string weather = makeRq($"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={latitude}&longitude={longitude}&hourly=pm10,pm2_5,carbon_monoxide,carbon_dioxide,nitrogen_dioxide,sulphur_dioxide,ozone");
+
+				string info = weather.Substring(weather.Substring(1, weather.Length - 2).IndexOf('{'), weather.Length - 2 - weather.Substring(1, weather.Length - 2).IndexOf('{'));
+				string units = info.Substring(2, info.IndexOf('}') - 3);
+
+				// times
+				string data = info.Substring(info.IndexOf('}') + 11, info.Length - info.IndexOf('}') - 11);
+				string hour = data.Substring(data.IndexOf('[') + 1, data.IndexOf(']') - data.IndexOf('[') - 1);
+
+				Times = hour.Split(',').ToList();
+
+				List<string> pollutentData = new List<string>();
+				List<string> pollutentNames = new List<string> { "Particulate matter PM10", "Particulate matter PM2.5", "Carbon monoxide", "Carbon dioxide", "Nitrogen dioxide", "Sulpher dioxide" };
+
+				string add = data.Substring(data.IndexOf('[') + 1, data.Length - data.IndexOf('[') - 3);
+				add = add.Substring(add.IndexOf(']') + 1, add.Length - add.IndexOf(']') - 1);
+
+				File.WriteAllText("output.txt", add);
+
+				while (true)
+				{
+					try
+					{
+						bool inside = false; string dataToAdd = "";
+
+						foreach (char item in add)
+						{
+							if (item == '[' || item == ']')
+							{
+								if (inside)
+								{
+									inside = false;
+									pollutentData.Add(dataToAdd);
+									dataToAdd = "";
+								}
+								else { inside = true; }
+								continue;
+							}
+							if (inside)
+							{
+								dataToAdd += item;
+							}
+						}
+						break;
+					}
+					catch
+					{
+						break;
+					}
+				}
+
+				for (int i = 0; i < pollutentData.Count; i++)
+				{
+					PollutentData[pollutentNames[i]] = pollutentData[i];
+				}
+
+			}
+			catch (ArgumentOutOfRangeException) { }
+		}
+	}
+
+}
